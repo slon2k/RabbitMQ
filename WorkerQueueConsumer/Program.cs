a6c63575c272b993f884ab7a5ac9b8f6e1112e5a
Boris Korobeinikov <slon2k@gmail.com>
2020-08-09 10:44:48 +0600

    Worker Queue

---
+++ WorkerQueueConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Common;
+using System;
+
+namespace WorkerQueueConsumer
+{
+    public class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+        private const string QueueName = "WorkerQueue";
+
+        static void Main()
+        {
+            CreateConnection();
+
+            var consumer = new EventingBasicConsumer(_model);
+            consumer.Received += (sender, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var payment = body.Deserialize<Payment>();
+                Console.WriteLine(@$"Received from {payment.Name} card: {payment.CardNumber} ${payment.Amount}");
+                _model.BasicAck(ea.DeliveryTag, multiple: false);
+            };
+            _model.BasicConsume(QueueName, autoAck: false, consumer);
+            Console.ReadLine();
+        }
+
+        private static void CreateConnection()
+        {
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+    }
+}
---
+++ WorkerQueueProducer/Program.cs
@@ -0,0 +1,43 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Common;
+using System;
+using System.Threading;
+
+namespace WorkerQueueProducer
+{
+    public class Program
+    {
+        private static ConnectionFactory _factory;
+        private static IConnection _connection;
+        private static IModel _model;
+
+        private const string QueueName = "WorkerQueue";
+        static void Main()
+        {
+            CreateConnection();
+
+            for (int i = 1; i < 100; i++)
+            {
+                SendMessage(new Payment(i));
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void CreateConnection()
+        {
+            _factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            _connection = _factory.CreateConnection();
+            _model = _connection.CreateModel();
+
+            _model.QueueDeclare(QueueName, true, false, false, null);
+        }
+
+        private static void SendMessage(Payment payment)
+        {
+            _model.BasicPublish("", QueueName, null, payment.Serialize());
+            Console.WriteLine($@"Payment sent {payment.Name} card: {payment.CardNumber} ${payment.Amount}");
+        }
+    }
+}
