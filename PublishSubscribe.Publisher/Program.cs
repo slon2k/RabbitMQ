a2cdd6496c4a8a7d494fcc91685bb246e976ca41
Boris Korobeinikov <slon2k@gmail.com>
2020-08-09 11:22:30 +0600

    PublishSubscribe

---
+++ PublishSubscribe.Publisher/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Common;
+using System;
+using System.Threading;
+
+namespace PublishSubscribe.Publisher
+{
+    public class Program
+    {
+        private const string ExchangeName = "PublishSubscribe";
+        
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(ExchangeName, type: ExchangeType.Fanout);
+                    
+                    for (int i = 1; i < 100; i++)
+                    {
+                        SendMessage(new Payment(i), channel);
+                        Thread.Sleep(1000);
+                    }
+
+                }
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static void SendMessage(Payment payment, IModel channel)
+        {
+            channel.BasicPublish(ExchangeName, "", null, payment.Serialize());
+            Console.WriteLine($@"Payment sent {payment.Name} card: {payment.CardNumber} ${payment.Amount}");
+        }
+    }
+}
---
+++ PublishSubscribe.Subscriber/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Common;
+using System;
+
+namespace PublishSubscribe.Subscriber
+{
+    public class Program
+    {
+        private const string ExchangeName = "PublishSubscribe";
+
+        static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "guest", Password = "guest" };
+            
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Fanout);
+
+                    var queueName = channel.QueueDeclare().QueueName;
+                    
+                    channel.QueueBind(queue: queueName, exchange: ExchangeName, routingKey: "");
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var payment = body.Deserialize<Payment>();
+                        Console.WriteLine(@$"Received from {payment.Name} card: {payment.CardNumber} ${payment.Amount}");
+                    };
+
+                    channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+            
+        }
+    }
+}
